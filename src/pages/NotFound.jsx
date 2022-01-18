4f2d06ecc6a3055b95744013497176fe2ae3e44a
Sergei Palmer <zigpalmer@gmail.com>
2022-01-18 16:54:04 +0000

    Made 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,23 @@
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      Not Found
+    <div className="hero">
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+          <h1 className="text-8xl font-bold mb-8">
+            I think you took a wrong turn.
+          </h1>
+          <p className="text-5xl mb-8">404 - Page not found!</p>
+          <Link to="/" className="btn btn-primary btn-lg">
+            <FaHome className="mr-2" />
+            Go back home
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
