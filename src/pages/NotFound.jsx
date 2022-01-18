3056011eb58a273bce7142b4053552ff63dafc73
Sergei Palmer <zigpalmer@gmail.com>
2022-01-18 16:54:28 +0000

    Updated text sizes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,10 +6,10 @@ const NotFound = () => {
     <div className="hero">
       <div className="text-center hero-content">
         <div className="max-w-lg">
-          <h1 className="text-8xl font-bold mb-8">
+          <h1 className="text-6xl font-bold mb-8">
             I think you took a wrong turn.
           </h1>
-          <p className="text-5xl mb-8">404 - Page not found!</p>
+          <p className="text-4xl mb-8">404 - Page not found!</p>
           <Link to="/" className="btn btn-primary btn-lg">
             <FaHome className="mr-2" />
             Go back home
