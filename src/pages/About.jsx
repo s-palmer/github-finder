2f6b989ce5b6e481e5e5dd83393dc83c7d4a1919
Sergei Palmer <zigpalmer@gmail.com>
2022-01-18 16:41:10 +0000

    Added basic routes for Home, About, Not Found pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">This is an app built in React that can be used to search Github profiles and see project details.</h1>
+      <p>Built by <a class="text-decoration-line: underline" href="https://github.com/s-palmer">Sergei Palmer</a></p>
+
+    </div>
+  )
+}
+
+export default About
