cf0cc95bc2959055f663a8af1f059607ef4da6f6
Sergei Palmer <zigpalmer@gmail.com>
2022-01-19 14:08:28 +0000

    Added conditional to clear button

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 const UserSearch = () => {
   const [text, setText] = useState("");
+
+  const { users } = useContext(GithubContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -10,8 +13,8 @@ const UserSearch = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(text === "") {
-      alert("Enter a search")
+    if (text === "") {
+      alert("Enter a search");
     } else {
       // @todo add user search function
     }
@@ -42,9 +45,11 @@ const UserSearch = () => {
           </div>
         </form>
       </div>
-      <div>
-        <button className="btn btn-ghost btn-lg">Clear</button>
-      </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        </div>
+      )}
     </div>
   );
 };
