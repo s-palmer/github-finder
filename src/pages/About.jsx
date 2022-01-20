ae63c5199675cb390cf25d93247c6377b3ad0aba
Sergei Palmer <zigpalmer@gmail.com>
2022-01-20 08:16:32 +0000

    Added target and rel to a tag on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,6 +10,8 @@ const About = () => {
         <a
           href="https://github.com/s-palmer"
           className="text-decoration-line: underline"
+          target="_blank"
+          rel="noreferrer"
         >
           Sergei Palmer
         </a>
